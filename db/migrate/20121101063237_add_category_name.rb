6482e26c36ee5ab2eb70ce3570f29148110429e6
virtual-force <nabeel.nizami@virtual-force.com>
2012-11-02 10:59:59 +0500

    “init”

--- db/migrate/20121101063237_add_category_name.rb
+++ db/migrate/20121101063237_add_category_name.rb
@@ -1,15 +1,35 @@
+#class AddCategoryName < ActiveRecord::Migration
+#  def up
+##    execute "insert into categories (category_name) values ('Food')"
+##    execute "insert into categories (category_name) values ('Cars')"
+##    execute "insert into categories (category_name) values ('Computers')"
+##    execute "insert into categories (category_name) values ('Smart Phones')"
+##    execute "insert into categories (category_name) values ('Trevel')"
+##    execute "insert into categories (category_name) values ('Education')"
+##    execute "insert into categories (category_name) values ('Health')"
+#  end
+#
+#  def down
+#    CategoryName.delete_all
+#  end
+#end
+
+
+
 class AddCategoryName < ActiveRecord::Migration
-  def up
-    execute "insert into categories (category_name) values ('Food')"
-    execute "insert into categories (category_name) values ('Cars')"
-    execute "insert into categories (category_name) values ('Computers')"
-    execute "insert into categories (category_name) values ('Smart Phones')"
-    execute "insert into categories (category_name) values ('Trevel')"
-    execute "insert into categories (category_name) values ('Education')"
-    execute "insert into categories (category_name) values ('Health')"
+  def self.up
+
+    Category.create :category_name => "Food"
+    Category.create :category_name => "Cars"
+    Category.create :category_name => "Computers"
+    Category.create :category_name => "Smart Phones"
+    Category.create :category_name => "Trevel"
+    Category.create :category_name => "Education"
+    Category.create :category_name => "Health"
+    Category.create :category_name => "Media"
   end
 
-  def down
-    CategoryName.delete_all
+  def self.down
+      drop_table :categories
   end
 end
