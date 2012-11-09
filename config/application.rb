cb7fb645631901b899f392dfeb4a1c87a0c0afcc
virtual-force <nabeel.nizami@virtual-force.com>
2012-11-09 14:50:30 +0500

    initial

--- config/application.rb
+++ config/application.rb
@@ -52,8 +52,8 @@ module Blog
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
-    config.assets.initialize_on_precompile = false
-    config.assets.precompile += ['960sm.css']
+#    config.assets.initialize_on_precompile = false
+#    config.assets.precompile += ['960sm.css']
     # Enable the asset pipeline
     config.assets.enabled = true
 
