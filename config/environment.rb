94ff2a93904a0a3728628bfde89941b695514971
virtual-force <nabeel.nizami@virtual-force.com>
2012-11-13 15:44:40 +0500

    changed

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 
-
+::ActiveSupport::Deprecation.silenced = true
 # Initialize the rails application
 Blog::Application.initialize!
 #config.gem(
